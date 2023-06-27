d778945e43e191d31ebc5b20a0ae6da26ed578f7
ohussar <pedrogabrielhussar133@gmail.com>
2023-06-27 16:20:12 -0300

    created block register

--- src/main/java/com/ohussar/mysticalarcane/Main.java
+++ src/main/java/com/ohussar/mysticalarcane/Main.java
@@ -1,14 +1,9 @@
 package com.ohussar.mysticalarcane;
 
 import com.mojang.logging.LogUtils;
-import net.minecraft.client.Minecraft;
-import net.minecraft.world.item.BlockItem;
-import net.minecraft.world.item.CreativeModeTab;
+import com.ohussar.mysticalarcane.Content.Blocks;
+
 import net.minecraft.world.item.Item;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.Blocks;
-import net.minecraft.world.level.block.state.BlockBehaviour;
-import net.minecraft.world.level.material.Material;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
@@ -16,7 +11,6 @@ import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -31,7 +25,6 @@ public class Main
     // Directly reference a slf4j logger
     private static final Logger LOGGER = LogUtils.getLogger();
     // Create a Deferred Register to hold Blocks which will all be registered under the "examplemod" namespace
-    public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, MODID);
     // Create a Deferred Register to hold Items which will all be registered under the "examplemod" namespace
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, MODID);
     public Main()
@@ -40,9 +33,9 @@ public class Main
 
         // Register the commonSetup method for modloading
         modEventBus.addListener(this::commonSetup);
-
+        Blocks.registerBlocks(modEventBus);
+        LOGGER.info("Registered blocks from " + MODID + "!");
         // Register the Deferred Register to the mod event bus so blocks get registered
-        BLOCKS.register(modEventBus);
         // Register the Deferred Register to the mod event bus so items get registered
         ITEMS.register(modEventBus);
 
@@ -51,19 +44,9 @@ public class Main
     }
 
     private void commonSetup(final FMLCommonSetupEvent event)
-    {
-        // Some common setup code
-        LOGGER.info("HELLO FROM COMMON SETUP");
-        LOGGER.info("DIRT BLOCK >> {}", ForgeRegistries.BLOCKS.getKey(Blocks.DIRT));
-    }
+    {}
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
-    @SubscribeEvent
-    public void onServerStarting(ServerStartingEvent event)
-    {
-        // Do something when the server starts
-        LOGGER.info("HELLO from server starting");
-    }
 
     // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
     @Mod.EventBusSubscriber(modid = MODID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
@@ -72,9 +55,6 @@ public class Main
         @SubscribeEvent
         public static void onClientSetup(FMLClientSetupEvent event)
         {
-            // Some client setup code
-            LOGGER.info("HELLO FROM CLIENT SETUP");
-            LOGGER.info("MINECRAFT NAME >> {}", Minecraft.getInstance().getUser().getName());
         }
     }
 }
--- src/main/java/com/ohussar/mysticalarcane/content/Blocks.java
+++ src/main/java/com/ohussar/mysticalarcane/content/Blocks.java
@@ -1,2 +1,17 @@
-package com.ohussar.mysticalarcane.content;
+package com.ohussar.mysticalarcane.Content;
 
+import com.ohussar.mysticalarcane.Main;
+
+import net.minecraft.world.level.block.Block;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+
+public class Blocks {
+    public static final DeferredRegister<Block> BLOCKS = 
+    DeferredRegister.create(ForgeRegistries.BLOCKS, Main.MODID);
+
+    public static void registerBlocks(IEventBus bus){
+        BLOCKS.register(bus);
+    }
+}
