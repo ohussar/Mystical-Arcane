b5852bdfcc1263e726a2a24cb575f338bc691bde
ohussar <pedrogabrielhussar133@gmail.com>
2023-06-27 18:46:13 -0300

    Revert "Delete src/main/java/com/ohussar/mysticalarcane/content directory"
    This reverts commit 5420a3c493df0af5e7a2a586116c9c09c1659367.

---
+++ src/main/java/com/ohussar/mysticalarcane/content/Blocks.java
@@ -0,0 +1,21 @@
+package com.ohussar.mysticalarcane.Content;
+
+import com.ohussar.mysticalarcane.Main;
+
+
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+public class Blocks {
+    public static final DeferredRegister<Block> BLOCKS = 
+    DeferredRegister.create(ForgeRegistries.BLOCKS, Main.MODID);
+    public static final RegistryObject<Block> ITEM_ALTAR = BLOCKS.register("itemaltar", 
+    () -> new ItemAltarBlock(BlockBehaviour.Properties.copy(net.minecraft.world.level.block.Blocks.IRON_BLOCK)));
+    public static void registerBlocks(IEventBus bus){
+        BLOCKS.register(bus);
+    }
+}
